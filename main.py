6a63c3c181406508ca5d4c0fa58e30aff5950237
Artem Tverdohleb <artemtverdohleb0@gmail.com>
2022-10-20 17:54:42 +0300

    wrote full functions to run the interpreture

--- main.py
+++ main.py
@@ -14,3 +14,12 @@ def get_second_name():
 def get_last_name():
     last_name = names.get_last_name()
     return last_name
+
+
+if __name__ == "__main__":
+    first = get_first_name()
+    print(f"My first name is:{first}")
+    second = get_second_name()
+    print(f"My second name is:{second}")
+    last = get_last_name()
+    print(f"My last name is:{last}")
