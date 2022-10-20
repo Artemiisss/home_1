ca924b405bfc06a9d9de74128b6173ea1fb5c260
Artem Tverdohleb <artemtverdohleb0@gmail.com>
2022-10-20 17:46:00 +0300

    added second name to  male person

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ import names
 def get_first_name():
     first_name = names.get_first_name(gender='male')
     return first_name
+
+
+def get_second_name():
+    second_name = names.get_full_name(gender='male')
+    return second_name
+
+
+
