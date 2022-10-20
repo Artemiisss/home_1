e3a81faa2a1865e65fde72c829f825c26f2c2c7e
Artem Tverdohleb <artemtverdohleb0@gmail.com>
2022-10-20 19:07:36 +0300

    fixed a last commit by adding a function call

--- main.py
+++ main.py
@@ -6,7 +6,20 @@ def get_first_name():
     return first_name
 
 
+def get_second_name():
+    second_name = names.get_full_name(gender='male')
+    return second_name
+
+
+def get_last_name():
+    last_name = names.get_last_name()
+    return last_name
+
+
 if __name__ == "__main__":
     first = get_first_name()
     print(f"My first name is:{first}")
-
+    second = get_second_name()
+    print(f"My second name is:{second}")
+    last = get_last_name()
+    print(f"My last name is:{last}")
